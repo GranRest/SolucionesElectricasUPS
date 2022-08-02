d7b132220c624443d388a65d70660e8bd2bc5aaa
Luis Anrrango <granresttecnology2021@gmail.com>
2022-08-02 12:05:47 -0500

    Vista Detalle paso de Datos pero no muestra

--- src/app/components/detallecatalogo/detallecatalogo.component.ts
+++ src/app/components/detallecatalogo/detallecatalogo.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { Router,ActivatedRoute,Params } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Producto } from 'src/app/models/Productos';
+import { Bateria } from 'src/app/models/Baterias';
+
+import { ProductsService } from './../../products.service';
 
 @Component({
   selector: 'app-detallecatalogo',
   templateUrl: './detallecatalogo.component.html',
-  styleUrls: ['./detallecatalogo.component.css']
+  styleUrls: ['./detallecatalogo.component.css'],
 })
 export class DetallecatalogoComponent implements OnInit {
+  // product: Producto = {} as Producto;
 
-public id: String="";
-
-  constructor(private _route: ActivatedRoute,
-    private _router: Router) { }
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private servicioProductos: ProductsService
+  ) {}
+  public listabaterias: Array<any> = [];
 
   ngOnInit(): void {
-    this._route.params.subscribe((params:Params) =>{
-      console.log(params);
+    this.servicioProductos.disparadorDeProductos.subscribe((data) => {
+      console.log('Recibiendo data..', data);
+      this.listabaterias.push(data);
+      console.log('Datos en ArrayList', this.listabaterias);
     });
   }
- 
-
 }
